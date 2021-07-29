060bdbd1b3c093af1bcfdd1c1784040ce6a42a16
ShaazShaaz <shaziasenol@gmail.com>
2021-07-29 14:34:26 -0400

    one more

--- src/day3/ListPractice.java
+++ src/day3/ListPractice.java
@@ -6,5 +6,6 @@ public class ListPractice {
     public static void main(String[] args) {
         ArrayList<String> str=new ArrayList<>();
         // adding another commit
+        /// ....
     }
 }
